d6966015a99be196ac048df262d1bc214507f7f3
tedsuo <ted@radicaldesigns.org>
2014-09-01 16:10:50 -0700

    proxy package initial check-in

---
+++ proxy/proxy.go
@@ -0,0 +1,28 @@
+package proxy
+
+import (
+	"os"
+
+	"github.com/tedsuo/ifrit"
+)
+
+func New(proxySignals <-chan os.Signal, runner ifrit.Runner) ifrit.Runner {
+	return ifrit.RunFunc(func(signals <-chan os.Signal, ready chan<- struct{}) error {
+		process := ifrit.Envoke(runner)
+		go forwardSignals(proxySignals, process)
+		go forwardSignals(signals, process)
+		return <-process.Wait()
+	})
+}
+
+func forwardSignals(signals <-chan os.Signal, process ifrit.Process) {
+	exit := process.Wait()
+	for {
+		select {
+		case sig := <-signals:
+			process.Signal(sig)
+		case <-exit:
+			return
+		}
+	}
+}
---
+++ proxy/proxy_test.go
@@ -0,0 +1,12 @@
+package proxy_test
+
+import (
+	. "github.com/tedsuo/ifrit/proxy"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Proxy", func() {
+
+})
